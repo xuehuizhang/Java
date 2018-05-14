2dc9c598637a8832984e6bd89663c89d686fe03f
zhangxuehui <zhangxuehui@beisen.com>
2018-05-14 16:46:31 +0800

    添加c3p0增删改查代码

--- JDBCProject/src/zuehui/test/Test1.java
+++ JDBCProject/src/zuehui/test/Test1.java
@@ -29,4 +29,47 @@ public class Test1 {
             ex.printStackTrace();
         }
     }
+    @Test
+    public void testDel(){
+        Connection conn=null;
+        PreparedStatement pstmt=null;
+        try{
+            conn=MyDataSource.getConnection();
+            String sql="delete from userinfo where uid=?";
+            pstmt=conn.prepareStatement(sql);
+            pstmt.setInt(1,1);
+            int n=pstmt.executeUpdate();
+            if(n>0){
+                System.out.println("删除成功");
+            }else{
+                System.out.println("删除失败");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testEdit(){
+
+    }
+    @Test
+    public void testSelect(){
+        Connection conn=null;
+        PreparedStatement pstmt=null;
+        ResultSet rs=null;
+        try{
+            conn=MyDataSource.getConnection();
+            String sql="select * from userinfo";
+            pstmt=conn.prepareStatement(sql);
+            rs=pstmt.executeQuery();
+            while(rs.next()){
+                System.out.println("uId="+rs.getInt(1)+",uName="+rs.getString(2)+"uPwd="+
+                rs.getString(3));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
